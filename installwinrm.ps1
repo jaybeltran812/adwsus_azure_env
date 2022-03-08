0fd90c694edffe106fbeb701ef0663ae5193fe76
jaybeltran812 <94095944+jaybeltran812@users.noreply.github.com>
2022-03-08 12:25:38 +0800

    Update installwinrm.ps1

--- installwinrm.ps1
+++ installwinrm.ps1
@@ -2,4 +2,3 @@ Enable-PSRemoting -force;
 winrm set winrm/config '@{MaxTimeoutms="1800000"}';
 winrm set winrm/config/service '@{AllowUnencrypted="true"}';
 winrm set winrm/config/service/auth '@{Basic="true"}';
-netsh advfirewall firewall add rule name=”WinRM-HTTP” dir=in localport=5986 protocol=TCP action=allow;
